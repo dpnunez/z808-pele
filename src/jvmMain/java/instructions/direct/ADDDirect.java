4ab86c21e3ba33e90ff305bca64fc0a093f2c36e
MarceloFaiz <MarceloPetitotR@gmail.com>
2023-07-05 19:51:00 -0300

    criação das classes de ADD.

---
+++ src/jvmMain/java/instructions/direct/ADDDirect.java
@@ -0,0 +1,39 @@
+package instructions.direct;
+
+
+import instructions.Instruction;
+import main.Memory;
+import main.Register;
+import main.Registers;
+
+public class ADDDirect extends Instruction {
+
+    public ADDDirect() {
+        super("ADD-Direct", (short) 0x05, 3);
+    }
+
+    public void execute(Registers registers, Memory memory, Short op) {
+        Register regDestination = registers.getRegisterByName("AX");
+        short value = memory.getCell(op);
+        Register regFlag = registers.getFlagRegister();
+        short flags = (short) 0x0000;
+
+        regDestination.setValue((short)(regDestination.getValue() + value));
+
+        // of & cf
+        if (regDestination.getValue() < -16384 || regDestination.getValue() > 16384) {
+            flags += 0x1000; //1*2^12 decimal = 4096
+            flags += 0x01; //1*2^0 decimal = 1
+        }
+        // zf
+        if (regDestination.getValue() == 0) {
+            flags += 0x0100; //1*2^8 decimal = 256
+        }
+        // pf
+        if (regDestination.getBitParity())  {
+            flags += 0x0040; //1*2^6 decimal = 64
+        }
+        regFlag.setValue(flags);
+
+    }
+}
--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -26,7 +26,7 @@ fun App() {
 
 
     val sb = Sandbox()
-    sb.orDirect();
+    sb.addImediate();
 
     Scaffold(
         topBar = {
