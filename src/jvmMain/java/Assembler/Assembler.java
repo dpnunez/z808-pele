72bc572be876973ac0b2a52a7e98a9d929907088
Daniel Pôrto Núñez <daniel.portonunez@gmail.com>
2023-07-28 00:25:04 -0300

    feat: add line spliter

--- src/jvmMain/java/Assembler/Assembler.java
+++ src/jvmMain/java/Assembler/Assembler.java
@@ -2,6 +2,13 @@ package Assembler;
 
 public class Assembler {
     public void run(String code) {
-        System.out.println("Assembler running code: " + code);
+        String[] lines = code.split("\n");
+        int count = lines.length;
+
+        // passar por todas as linhas e printar
+        for (int i = 0; i < count; i++) {
+            System.out.println(lines[i]);
+            System.out.println("Linha " + i);
+        }
     }
 }
